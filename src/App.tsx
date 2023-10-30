f75a4e026e54b798365694a0f39ab60240c1b27b
shiv <mansotra.shiv@gmail.com>
2023-10-30 09:58:22 +0530

    added axiosInstance

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
 
 function App() {
 
@@ -9,7 +7,7 @@ function App() {
     <>
       Hii, shotu
     </>
-  )
+  );
 }
 
-export default App
+export default App;
